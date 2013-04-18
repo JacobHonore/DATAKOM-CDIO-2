275545bd8bac79659f36c60a49ca6e43d888da16
Jesper <jespervmark@gmail.com>
2013-04-18 16:01:11 +0200

    dsandsadsa

--- Datakom CDIO 2/src/weigth/TCPClient.java
+++ Datakom CDIO 2/src/weigth/TCPClient.java
@@ -2,19 +2,18 @@ package weigth;
 
 import java.io.*; 
 import java.net.*; 
+import java.util.Calendar;
 class TCPClient { 
 
-	private String sentence, modifiedSentence, txt, input;
-	private int itemNo, tempInt;
-	private String[] temp = new String[10];
-
+	private String weightMsg, oprID, serverInput, itemName, userInput;
+	private int itemNoInput, itemNoStore;
+	private String[] splittedInput = new String[10];
+	private double tara, netto, bruttoCheck;
 
 
 	public static void main(String[] args) throws Exception { 
 
 		TCPClient t = new TCPClient();
-		BufferedReader inFromUser = 
-				new BufferedReader(new InputStreamReader(System.in)); 
 
 		Socket clientSocket = new Socket("localhost", 4567); 
 
@@ -27,28 +26,27 @@ class TCPClient {
 		BufferedReader inFromLocal = new BufferedReader(new FileReader(new File("store.txt")));
 
 		t.sekvens1(inFromLocal, inFromServer, outToServer);
+
+		clientSocket.close();
 	}
 
 
 
 	public void sekvens1(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
 	{
-		txt = "Indtast ID";
-			outToServer.writeBytes("RM20 8 \"" + txt + "\" \" \" \"&3\"\r\n");
-			this.sekvens2(inFromLocal, inFromServer, outToServer);
+		weightMsg = "Indtast ID";
+		outToServer.writeBytes("RM20 8 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+		this.sekvens2(inFromLocal, inFromServer, outToServer);
 	}
 	public void sekvens2(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
 	{
-		String temp2 = inFromServer.readLine();
-		//		temp2 = inFromServer.readLine();
-		//		temp2 = inFromServer.readLine();
-		System.out.println("" + temp2);
-		if(temp2.equals("RM20 B")){
-			System.out.println("" + temp2);
-			temp2 = inFromServer.readLine();
-			temp = temp2.split(" ");
-			input = temp[2];		
-			System.out.println("" + input);
+		serverInput = inFromServer.readLine();
+		//		serverInput = inFromServer.readLine();
+		//		serverInput = inFromServer.readLine();
+		if(serverInput.equals("RM20 B")){
+			serverInput = inFromServer.readLine();
+			splittedInput = serverInput.split(" ");
+			oprID = splittedInput[2];		
 			this.sekvens3(inFromLocal, inFromServer, outToServer);
 		}
 	}
@@ -57,21 +55,19 @@ class TCPClient {
 	public void sekvens3(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
 	{
 		//Sekvens 3 kunne kombineres med sekvens 4.
-		txt = "Indtast varenummer";
-		outToServer.writeBytes("RM20 4 \"" + txt + "\" \" \" \"&3\"\r\n");
+		weightMsg = "Indtast varenummer";
+		outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
 		this.sekvens4(inFromLocal, inFromServer, outToServer);
 	}
 
 	public void sekvens4(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
 	{
-		String temp2 = inFromServer.readLine();
-		temp = temp2.split(" ");
-		System.out.println("" + temp2);
-		if(temp2.equals("RM20 B")){
-			temp2 = inFromServer.readLine();
-			temp = temp2.split(" ");
-			itemNo = Integer.parseInt(temp[2]);
-			System.out.println("" + itemNo);
+		serverInput = inFromServer.readLine();
+		splittedInput = serverInput.split(" ");
+		if(serverInput.equals("RM20 B")){
+			serverInput = inFromServer.readLine();
+			splittedInput = serverInput.split(" ");
+			itemNoInput = Integer.parseInt(splittedInput[2]);
 			this.sekvens5(inFromLocal, inFromServer, outToServer);
 		}
 	}
@@ -80,32 +76,30 @@ class TCPClient {
 	{
 		boolean notFound = true;
 		while(notFound){
-			temp = inFromLocal.readLine().split(",");
-			tempInt = Integer.parseInt(temp[0]);
+			splittedInput = inFromLocal.readLine().split(",");
+			itemNoStore = Integer.parseInt(splittedInput[0]);
 
-			if(tempInt == itemNo){
+			if(itemNoStore == itemNoInput){
 				notFound = false;
-				input = temp[1];
-				txt = "Vare: " + input + "\nTast 1 for OK eller 0 for Annuller"; 
-				outToServer.writeBytes("RM20 4 \"" + txt + "\" \" \" \"&3\"\r\n");
+				itemName = splittedInput[1];
+				weightMsg = "Vare: " + itemName + "\nTast 1 for OK eller 0 for Annuller."; 
+				outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
 
-				String temp2 = inFromServer.readLine();
-				//		temp2 = inFromServer.readLine();
-				//		temp2 = inFromServer.readLine();
-				temp = temp2.split(" ");
-				System.out.println("" + temp2);
-				if(temp2.equals("RM20 B"))
+				serverInput = inFromServer.readLine();
+				//		serverInput = inFromServer.readLine();
+				//		serverInput = inFromServer.readLine();
+				splittedInput = serverInput.split(" ");
+				if(serverInput.equals("RM20 B"))
 				{
-					temp2 = inFromServer.readLine();
-					temp = temp2.split(" ");
-					input = temp[2];		
-					System.out.println("" + input);
-					if(input.equals("1"))
+					serverInput = inFromServer.readLine();
+					splittedInput = serverInput.split(" ");
+					userInput = splittedInput[2];		
+					if(userInput.equals("1"))
 					{
-						this.sekvens6(inFromLocal, inFromServer, outToServer);
+						this.sekvens7(inFromLocal, inFromServer, outToServer);
 					}
 					//Fejl: Kan annullere, men kan derefter ikke vælge samme vare igen.
-					else if(input.equals("0"))
+					else if(userInput.equals("0"))
 					{
 						this.sekvens3(inFromLocal, inFromServer, outToServer);
 					}
@@ -114,79 +108,172 @@ class TCPClient {
 		}
 	}
 
-	public void sekvens6(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
-	{
-		txt = "Anbring eventuelt en skaal og tarer.";
-		outToServer.writeBytes("RM20 4 \"" + txt + "\" \" \" \"&3\"\r\n");
-		this.sekvens7(inFromLocal, inFromServer, outToServer);
-	}
-
 	public void sekvens7(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
 	{
-		txt = "Vaegten tareres";
-		outToServer.writeBytes("RM46 1 0 \"" + txt + "\" \" \" \"&3\"\r\n");
+		weightMsg = "Anbring evt. skål og tast enter.";
+		outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+		serverInput = inFromServer.readLine();
+
+		if(serverInput.equals("RM20 B"))
+			serverInput = inFromServer.readLine();
+		else
+			this.sekvens7(inFromLocal, inFromServer, outToServer);
+
+		if(serverInput.startsWith("RM20 A"))
+			this.sekvens8(inFromLocal, inFromServer, outToServer);
+
 	}
+
+	public void sekvens8(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException
+	{
+		outToServer.writeBytes("T\r\n");
+
+		serverInput = inFromServer.readLine();
+		if(serverInput.startsWith("T S")){
+			splittedInput = serverInput.split(" +");
+			tara = Double.parseDouble(splittedInput[2]);
+			this.sekvens9(inFromLocal, inFromServer, outToServer);
+		}
+		else this.sekvens7(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens9(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+		weightMsg = "Påfyld vare og tast enter.";
+		outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+		serverInput = inFromServer.readLine();
+
+		if(serverInput.equals("RM20 B"))
+			serverInput = inFromServer.readLine();
+		else
+			this.sekvens9(inFromLocal, inFromServer, outToServer);
+
+		if(serverInput.startsWith("RM20 A"))
+			this.sekvens10(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens10(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+
+		outToServer.writeBytes("S\r\n");
+
+		serverInput = inFromServer.readLine();
+		if(serverInput.startsWith("S S")){
+			splittedInput = serverInput.split(" +");
+			netto = Double.parseDouble(splittedInput[2])-tara;
+			this.sekvens11(inFromLocal, inFromServer, outToServer);
+		}
+		else this.sekvens9(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens11(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+		weightMsg = "Fjern enheder fra vægt og tast enter.";
+		outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+		serverInput = inFromServer.readLine();
+
+
+		if(serverInput.equals("RM20 B"))
+			serverInput = inFromServer.readLine();
+		
+		else
+			this.sekvens11(inFromLocal, inFromServer, outToServer);
+		
+
+		if(serverInput.startsWith("RM20 A"))
+			this.sekvens12(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens12(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+	
+		weightMsg = "Vægt er tareret";
+		outToServer.writeBytes("D \""+weightMsg+"\"\r\n");
+		outToServer.writeBytes("T\r\n");
+		this.sekvens13(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens13(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+
+		serverInput = inFromServer.readLine();
+		if(serverInput.startsWith("T S")){
+			splittedInput = serverInput.split(" +");
+			bruttoCheck = Double.parseDouble(splittedInput[2]);
+			this.sekvens13(inFromLocal, inFromServer, outToServer);
+		}
+		else this.sekvens14(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void sekvens14(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+		if(bruttoCheck >= 2 || bruttoCheck <= -2){
+
+			weightMsg = "Afvejning afvist. Tast enter for at veje igen.";
+			outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+			serverInput = inFromServer.readLine();
+
+			if(serverInput.equals("RM20 B"))
+				serverInput = inFromServer.readLine();
+			else
+				this.sekvens14(inFromLocal, inFromServer, outToServer);
+
+			if(serverInput.startsWith("RM20 A"))
+				this.sekvens7(inFromLocal, inFromServer, outToServer);
+		}
+		else{
+
+			weightMsg = "Afvejning godkendt. Tast enter for at starte forfra.";
+			outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+			serverInput = inFromServer.readLine();
+
+			if(serverInput.equals("RM20 B"))
+				serverInput = inFromServer.readLine();
+			else
+				this.sekvens14(inFromLocal, inFromServer, outToServer);
+
+			if(serverInput.startsWith("RM20 A"))
+				this.sekvens15(inFromLocal, inFromServer, outToServer);
+		}
+	}
+
+	public void sekvens15(BufferedReader inFromLocal, BufferedReader inFromServer, DataOutputStream outToServer) throws IOException{
+
+		try{
+			log();
+		}
+		catch(FileNotFoundException e){
+			weightMsg = "Logning kunne ikke foretages. Tast enter for at starte forfra";
+			outToServer.writeBytes("RM20 4 \"" + weightMsg + "\" \" \" \"&3\"\r\n");
+
+			serverInput = inFromServer.readLine();
+
+			if(serverInput.equals("RM20 B"))
+				serverInput = inFromServer.readLine();
+			else
+				this.sekvens15(inFromLocal, inFromServer, outToServer);
+
+			if(serverInput.startsWith("RM20 A"))
+				this.sekvens1(inFromLocal, inFromServer, outToServer);
+		}
+
+
+		this.sekvens1(inFromLocal, inFromServer, outToServer);
+	}
+
+	public void log() throws IOException{
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("Log.txt")));
+		Calendar d = Calendar.getInstance();
+		String timeStamp = "" + d.get(Calendar.YEAR) + "-" + (d.get(Calendar.MONTH) + 1) + "-" + d.get(Calendar.DATE) + "   " + d.get(Calendar.HOUR_OF_DAY) + ":" + d.get(Calendar.MINUTE) + ":" + d.get(Calendar.SECOND);
+
+		bw.write(timeStamp + ", " + oprID + ", " + itemNoInput + ", " + itemName + ", " + netto + " kg.");
+		bw.flush();
+		bw.close();
+	}
+
+
 }
 
-
-	//		}
-	//		while();
-
-	//		try{
-	//			do{
-	//				System.out.println("Available commands: " +
-	//						"\nPress 1 for 'Read Scale'" +
-	//						"\nPress 2 for 'Tare Scale' " +
-	//						"\nPress 3 for 'Zero Scale' " +
-	//						"\nPress 4 for 'Display Text on Scale'" +
-	//						"\nPress 5 for 'Display Weight on Scale'" +
-	//						"\nPress 6 for 'Display Text on Scale, and recieve answer'" +
-	//						"\nPress 7 for 'Quit'");
-	//				sentence = inFromUser.readLine();
-	//
-	//				switch (sentence) {
-	//
-	//				case "1":
-	//					outToServer.writeBytes("S\r\n");
-	//					break;
-	//
-	//				case "2":
-	//					outToServer.writeBytes("T\r\n");
-	//					break;
-	//
-	//				case "3":
-	//					outToServer.writeBytes("Z\r\n");
-	//					break;
-	//
-	//				case "4":
-	//					outToServer.writeBytes("D hi \r\n");
-	//					break;
-	//
-	//				case "5":
-	//					outToServer.writeBytes("DW\r\n");
-	//					break;
-	//					
-	//				case "6":
-	//					System.out.println("Indtast forespørgsel: ");
-	//					String txt = inFromUser.readLine();
-	//					outToServer.writeBytes("RM20 8 \"" + txt + "\" \"\" \"&3\"\r\n");
-	//					break;
-	//
-	//				case "7":
-	//					System.out.println("Programmet lukkes");
-	//					outToServer.writeBytes("Q\r\n");
-	//					break;
-	//				}
-	//
-	//				modifiedSentence = inFromServer.readLine(); 
-	//
-	//				System.out.println("FROM SERVER: " + modifiedSentence); 
-	//			}
-	//			while(!sentence.equals("7"));	
-	//		}
-	//		catch(SocketException e){
-	//			System.out.println("Connection lost!");
-	//		}
 
 
 
